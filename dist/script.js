a31299cc96134d02db2c50c8a3c06d9bcdbcd019
Olym <aguineaolymm@gmail.com>
2023-08-28 17:05:44 +0800

    Calculator v0.4

--- dist/script.js
+++ dist/script.js
@@ -1,114 +1,30 @@
-const choices = ["Rock", "Paper", "Scissor"];
+let firstNumber = 5;
+let secondNumber = 5;
+let operator = ["+", "-", "*", "/"];
 
-let btnRock = document.getElementById("rock");
-let btnPaper = document.getElementById("paper");
-let btnScissor = document.getElementById("scissor");
-
-const div = document.createElement("div");
-div.className = "results";
-
-const announce = document.createElement("div");
-announce.className = "announce";
-
-const playerSide = document.createElement("div");
-const playerScoreBoard = document.createElement("p");
-playerSide.className = "playerSide";
-playerScoreBoard.textContent = "Player score: 0";
-
-const computerSide = document.createElement("div");
-const computerScoreBoard = document.createElement("p");
-computerSide.className = "computerSide";
-computerScoreBoard.textContent = "Computer score: 0";
-
-div.appendChild(announce);
-div.appendChild(playerSide);
-div.appendChild(computerSide);
-
-document.body.appendChild(div);
-
-let gameOver = false;
-let playerScore = 0;
-let computerScore = 0;
-
-function getComputerChoice() {
-  return (randomNum = choices[Math.floor(Math.random() * choices.length)]);
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
 }
 
-function checkGameOver() {
-  if (playerScore === 5 || computerScore === 5) {
-    if (playerScore > computerScore) {
-      announce.textContent = "PLAYER WINS";
-    } else if (playerScore < computerScore) {
-      announce.textContent = "COMPUTER WINS";
-    }
-    gameOver = true;
-    btnRock.removeEventListener("click", () => {});
-    btnPaper.removeEventListener("click", () => {});
-    btnScissor.removeEventListener("click", () => {});
+function operate(operator, firstNumber, secondNumber) {
+  if (operator === "+") {
+    return add(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    return subtract(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    return multiply(firstNumber, secondNumber);
+  } else if (operator === "/") {
+    return divide(firstNumber, secondNumber);
   }
 }
-function updateScoreBoard() {
-  playerScoreBoard.textContent = `Player score: ${playerScore}`;
-  computerScoreBoard.textContent = `Computer score: ${computerScore}`;
-}
-function playRound(playerSelection, computerSelection) {
-  if (gameOver) {
-    return;
-  }
 
-  playerSide.textContent = "The player chose: " + playerSelection;
-  computerSide.textContent = "The computer chose: " + computerSelection;
-  playerSide.appendChild(playerScoreBoard);
-  computerSide.appendChild(computerScoreBoard);
-
-  switch (playerSelection) {
-    case "Rock":
-      if (computerSelection === "Scissor") {
-        announce.textContent = "Player wins! Rock beats Scissor";
-        playerScore += 1;
-      } else if (computerSelection === "Paper") {
-        announce.textContent = "Computer wins! Rock beats Paper";
-        computerScore += 1;
-      } else {
-        announce.textContent = "Its a draw!";
-      }
-      break;
-    case "Paper":
-      if (computerSelection === "Rock") {
-        announce.textContent = "Player wins! Paper beats Rock";
-        playerScore += 1;
-      } else if (computerSelection === "Scissor") {
-        announce.textContent = "Computer wins! Rock beats Paper";
-        computerScore += 1;
-      } else {
-        announce.textContent = "Its a draw";
-      }
-      break;
-    case "Scissor":
-      if (computerSelection === "Paper") {
-        announce.textContent = "Player wins! Scissor beats Paper";
-        playerScore += 1;
-      } else if (computerSelection === "Rock") {
-        announce.textContent = "Computer wins! Rock beats Paper";
-        computerScore += 1;
-      } else {
-        announce.textContent = "Its a draw";
-      }
-  }
-  updateScoreBoard();
-  checkGameOver();
-}
-
-function game() {
-  document.addEventListener("click", (event) => {
-    const target = event.target;
-    if (target === btnRock) {
-      playRound("Rock", getComputerChoice());
-    } else if (target === btnPaper) {
-      playRound("Paper", getComputerChoice());
-    } else if (target === btnScissor) {
-      playRound("Scissor", getComputerChoice());
-    }
-  });
-}
-game();
+console.log(operate("*", 5, 5));
