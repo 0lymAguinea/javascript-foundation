f31371286061646e83b2cf6982ed9085bf1e8c93
Olym <aguineaolymm@gmail.com>
2023-09-03 15:07:14 +0800

    Calculator v0.7

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,14 @@
-let firstNumber = 5;
-let secondNumber = 5;
-let operator = ["+", "-", "*", "/"];
+let firstNumber = 0;
+let secondNumber = 0;
+let firstOperator = ["+", "-", "*", "/"];
+let secondOperator = ["+", "-", "*", "/"];
+
+let display = document.getElementById("display");
+let btnNumber = document.querySelectorAll("#number");
+let btnOperator = document.querySelectorAll("#operator");
+let btnAC = document.getElementById("allClear");
+let btnEqual = document.getElementById("equal");
+let displayValue = [];
 
 function add(a, b) {
   return a + b;
@@ -15,16 +23,51 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, firstNumber, secondNumber) {
-  if (operator === "+") {
-    return add(firstNumber, secondNumber);
-  } else if (operator === "-") {
-    return subtract(firstNumber, secondNumber);
-  } else if (operator === "*") {
-    return multiply(firstNumber, secondNumber);
-  } else if (operator === "/") {
-    return divide(firstNumber, secondNumber);
-  }
+function btnCalculator() {
+  btnNumber.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      display.textContent += btn.value;
+      displayValue.push(btn.value);
+    });
+  });
+  btnOperator.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      display.textContent += btn.value;
+      displayValue.push(btn.value);
+    });
+  });
+  btnAC.addEventListener("click", () => {
+    displayValue = [];
+    display.textContent = "";
+  });
+  btnEqual.addEventListener("click", equal);
 }
 
-console.log(operate("*", 5, 5));
+function equal() {
+  let input = display.textContent;
+  let fields = input.split(/[+\-*/]/);
+  let operator = input.match(/[+\-*/]/)[0];
+  let firstNumber = fields[0];
+  let secondNumber = fields[1];
+  let newDisplay = operate(operator, Number(firstNumber), Number(secondNumber));
+  display.textContent = newDisplay;
+  displayValue.push(newDisplay);
+  let oldNum = newDisplay;
+}
+btnCalculator();
+
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+      break;
+    case "-":
+      return subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      return multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      return divide(firstNumber, secondNumber);
+  }
+}
