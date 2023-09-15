6b7ec48327da1fcb51d8a8f848d15c23c0fa4cd5
Olym <aguineaolymm@gmail.com>
2023-09-15 23:54:52 +0800

    V0.9

--- dist/script.js
+++ dist/script.js
@@ -1,60 +1,110 @@
 let display = document.getElementById("display");
-let btnNumber = document.querySelectorAll("#number");
-let btnOperator = document.querySelectorAll("#operator");
-let btnAC = document.getElementById("allClear");
-let btnEqual = document.getElementById("equal");
 let displayValue = [];
-let displayNum = parseInt(display.textContent);
+const btnNumbers = document.querySelectorAll(".number");
+const btnOperators = document.querySelectorAll(".operator");
+const btnAllClear = document.getElementById("allClear");
+const btnDel = document.getElementById("del");
+const btnPeriod = document.getElementById("period");
+const btnEqual = document.getElementById("equal");
 
-function btnCalculator() {
-  btnNumber.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      display.textContent += btn.value;
-      displayNum = display.textContent;
-      displayValue.push(btn.value);
-    });
-  });
-  btnOperator.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      operateClick(btn);
-    });
-  });
-  btnAC.addEventListener("click", () => {
+function btnFunc() {
+  btnAllClear.addEventListener("click", () => {
+    display.textContent = "0";
     displayValue = [];
-    display.textContent = "";
+  });
+  btnDel.addEventListener("click", () => {
+    console.log("HI");
+  });
+  btnPeriod.addEventListener("click", () => {
+    const operatorRegex = /[+\-*/]/g;
+    let val = display.textContent.split(operatorRegex);
+    let n1 = val[0];
+    let n2 = val[1];
+    if (!n1.includes(".") || !n2.includes(".")) {
+      display.textContent += btnPeriod.value;
+      displayValue.push(btnPeriod.value);
+    }
+  });
+  btnNumbers.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const clickedButton = btn.value;
+      if (display.textContent === "0") {
+        display.textContent = clickedButton;
+        displayValue = [clickedButton];
+      } else {
+        display.textContent += btn.value;
+        displayValue.push(btn.value);
+      }
+    });
+  });
+  btnOperators.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let operatorClicked = btn.value;
+      if (
+        displayValue.length > 0 &&
+        "+-*/".includes(displayValue[displayValue.length - 1])
+      ) {
+        displayValue[displayValue.length - 1] = operatorClicked;
+        display.textContent = displayValue.join("");
+      } else {
+        display.textContent += operatorClicked;
+        displayValue.push(operatorClicked);
+        const operatorRegex = /[+\-*/]/g;
+        let input = display.textContent;
+        let prevOperator = input.match(operatorRegex)[0];
+        let currentOperator = input.match(operatorRegex)[1];
+        let fields = input.split(operatorRegex);
+        let firstNum = fields[0];
+        let secondNum = fields[1];
+        if (prevOperator && currentOperator) {
+          let result = operate(
+            prevOperator,
+            parseFloat(firstNum),
+            parseFloat(secondNum)
+          );
+          prevOperator = currentOperator;
+          firstNum = result;
+          secondNum = "";
+          display.textContent = firstNum + prevOperator;
+          displayValue = [];
+          displayValue.push(firstNum, prevOperator);
+        }
+      }
+    });
   });
   btnEqual.addEventListener("click", equal);
 }
-function operateClick(btn) {
-  let operatorClicked = btn.textContent;
-  if (
-    displayValue.length > 0 &&
-    "+-*/".includes(displayValue[displayValue.length - 1])
-  ) {
-    displayValue[displayValue.length - 1] = operatorClicked;
-    display.textContent = displayValue.join("");
-  } else {
-    display.textContent += operatorClicked;
-    displayValue.push(operatorClicked);
-  }
-}
+btnFunc();
 
 function equal() {
+  const operatorRegex = /[+\-*/]/g;
   let input = display.textContent;
-  let fields = input.split(/[+\-*/]/);
-  let operator = input.match(/[+\-*/]/)[0];
-  let firstNumber = fields[0];
-  let secondNumber = fields[1];
-  let newDisplay = operate(
-    operator,
-    parseInt(firstNumber),
-    parseInt(secondNumber)
-  );
-  display.textContent = newDisplay;
-  displayValue.push(newDisplay);
-  let oldNum = newDisplay;
+  let fields = input.split(operatorRegex);
+  let operator = input.match(operatorRegex)[0];
+  let firstNum = fields[0];
+  let secondNum = fields[1];
+  let result = operate(operator, parseFloat(firstNum), parseFloat(secondNum));
+  display.textContent = result;
+  displayValue = [];
+  displayValue.push(result);
 }
-btnCalculator();
+
+function operate(operator, firstNum, secondNum) {
+  switch (operator) {
+    case "+":
+      return add(firstNum, secondNum);
+      break;
+    case "-":
+      return subtract(firstNum, secondNum);
+      break;
+    case "*":
+      return multiply(firstNum, secondNum);
+      break;
+    case "/":
+      return divide(firstNum, secondNum);
+      break;
+  }
+}
 function add(a, b) {
   return a + b;
 }
@@ -66,20 +116,4 @@ function multiply(a, b) {
 }
 function divide(a, b) {
   return a / b;
-}
-
-function operate(operator, firstNumber, secondNumber) {
-  switch (operator) {
-    case "+":
-      return add(firstNumber, secondNumber);
-      break;
-    case "-":
-      return subtract(firstNumber, secondNumber);
-      break;
-    case "*":
-      return multiply(firstNumber, secondNumber);
-      break;
-    case "/":
-      return divide(firstNumber, secondNumber);
-  }
 }
