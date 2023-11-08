7214a47771033851b218957bb5d941f388fdafd4
lym <aguineaolymm@gmail.com>
2023-11-08 16:52:40 +0800

    0.8V Library

--- src/script.js
+++ src/script.js
@@ -6,30 +6,30 @@ function Book(title, author, pages, hasRead) {
   this.pages = pages;
   this.hasRead = hasRead;
 }
+
+function displayBook() {
+  const table = document.querySelector("table");
+
+  let row = table.insertRow(-1);
+  let title = row.insertCell(0);
+  let author = row.insertCell(1);
+  let pages = row.insertCell(2);
+
+  myLibrary.forEach((book) => {
+    title.innerHTML = book.title;
+    author.innerHTML = book.author;
+    pages.innerHTML = book.pages;
+  });
+}
+
 function addBookToLibrary() {
   const addButton = document.querySelector(".addBook");
-  const bookTitle = document.querySelector("#title");
-  const bookAuthor = document.querySelector("#author");
-  const bookPages = document.querySelector("#pages");
-  const bookHasReadYes = document.querySelector("#yes");
-  const bookHasReadNo = document.querySelector("#no");
-
-  const table = document.querySelector("table");
-  // let tdTitle = document.querySelector(".tdTitle");
-  // let tdAuthor = document.querySelector(".tdAuthor");
-  // let tdPages = document.querySelector(".tdPages");
-
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const pages = document.getElementById("pages");
   addButton.addEventListener("click", () => {
-    myLibrary.push(
-      `${bookTitle.value}, ${bookAuthor.value}, ${bookPages.value}`
-    );
-    let row = table.insertRow(-1);
-    let title = row.insertCell(0);
-    let author = row.insertCell(1);
-    let pages = row.insertCell(2);
-    title.innerHTML = bookTitle.value;
-    author.innerHTML = bookAuthor.value;
-    pages.innerHTML = bookPages.value;
+    myLibrary.push(new Book(title.value, author.value, pages.value));
+    displayBook();
   });
 }
 addBookToLibrary();
