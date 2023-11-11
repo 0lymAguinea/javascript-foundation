2197bb5dfa4f946f29f93299a786383568a68aeb
lym <aguineaolymm@gmail.com>
2023-11-11 15:44:24 +0800

    added delete button and read button

--- src/script.js
+++ src/script.js
@@ -9,16 +9,25 @@ function Book(title, author, pages, hasRead) {
 
 function displayBook() {
   const table = document.querySelector("table");
-
+  let readButton = document.createElement("button");
+  let delButton = document.createElement("button");
+  let bookID = 0;
   let row = table.insertRow(-1);
   let title = row.insertCell(0);
   let author = row.insertCell(1);
   let pages = row.insertCell(2);
-
+  let hasRead = row.insertCell(3);
+  let delCell = row.insertCell(4);
   myLibrary.forEach((book) => {
     title.innerHTML = book.title;
     author.innerHTML = book.author;
     pages.innerHTML = book.pages;
+    readButton.innerHTML = book.hasRead;
+    hasRead.appendChild(readButton);
+    delButton.innerHTML = book.delCell;
+    delCell.appendChild(delButton).innerText = "X";
+    delButton.dataset.bookid = bookID++;
+    removeBook();
   });
 }
 
@@ -27,9 +36,21 @@ function addBookToLibrary() {
   const title = document.getElementById("title");
   const author = document.getElementById("author");
   const pages = document.getElementById("pages");
+  const yesRadio = document.getElementById("yes");
+  const noRadio = document.getElementById("no");
+
   addButton.addEventListener("click", () => {
-    myLibrary.push(new Book(title.value, author.value, pages.value));
-    displayBook();
+    if (yesRadio.checked) {
+      myLibrary.push(
+        new Book(title.value, author.value, pages.value, yesRadio.value)
+      );
+      displayBook();
+    } else if (noRadio.checked) {
+      myLibrary.push(
+        new Book(title.value, author.value, pages.value, noRadio.value)
+      );
+      displayBook();
+    }
   });
 }
 addBookToLibrary();
