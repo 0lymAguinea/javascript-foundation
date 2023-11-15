4cd465c12d849866edce884bce12b55646881f92
0lym <aguineaolymm@gmail.com>
2023-11-15 16:27:50 +0800

    0.2.5V

--- src/script.js
+++ src/script.js
@@ -4,19 +4,13 @@ function gameBoard() {
     ["", "", ""],
     ["", "", ""],
   ];
-  return function (row, column, playerMarker) {
+  return function (row, column, playerMarker, player) {
     if (row >= 0 && row < 3 && column >= 0 && column < 3) {
       if (board[row][column] === "") {
         board[row][column] = playerMarker;
-        if (playerMarker === "X") {
-          console.log(
-            `${player1.name} Inserted ${playerMarker} at (${row}, ${column})`
-          );
-        } else if (playerMarker === "O") {
-          console.log(
-            `${player2.name} Inserted ${playerMarker} at (${row}, ${column})`
-          );
-        }
+        console.log(
+          `${player.name} Inserted ${playerMarker} at (${row}, ${column})`
+        );
       } else {
         console.log(`The location (${row}, ${column}) is already occupied.`);
       }
@@ -29,23 +23,18 @@ function gameBoard() {
   };
 }
 function createPlayer(name, marker) {
-  let player = { name, marker };
-  return player;
+  return { name, marker };
 }
 const player1 = createPlayer("OLYM", "X");
 const player2 = createPlayer("MYLO", "O");
 
-function insertMark() {
-  const board = gameBoard();
-  return function (row, column, playerMarker) {
-    let mark = board(row, column, playerMarker);
-    return mark;
-  };
+function insertMark(board, row, column, player) {
+  return board(row, column, player.marker, player);
 }
 function playGame() {
-  let playing = insertMark();
-  console.log(playing(1, 2, player1.marker));
-  console.log(playing(1, 1, player2.marker));
+  const board = gameBoard();
+  let playing = (row, column, player) => insertMark(board, row, column, player);
+  console.log(playing(1, 2, player1));
+  console.log(playing(1, 1, player2));
 }
-
 playGame();
