f4114cbcd87fd7fc748a7eaff456769201f1c141
lym <aguineaolymm@gmail.com>
2023-11-29 17:12:48 +0800

    js for todo class and todo array

--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -1,26 +1,15 @@
 //Responsible to creating todo items
-class createTodo {
-  constructor(
-    title,
-    description,
-    dueDate,
-    priority,
-    note = [],
-    checkList = []
-  ) {
+const myTodos = [];
+class Todo {
+  constructor(title, description, dueDate, priority, note = []) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.note = note;
-    this.checkList = checkList;
   }
 
   addNote(note) {
     this.note.push(note);
-  }
-
-  addItemToCheckList(checklist) {
-    this.checkList.push(checklist);
   }
 }
