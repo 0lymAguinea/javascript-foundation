600f39f2c48e75f73b3e03d39fa2d303bb0b0a3b
lym <aguineaolymm@gmail.com>
2023-11-30 17:14:15 +0800

    add bunch of function that display button containing information from form. Only the middle content is available now.

--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -13,3 +13,52 @@ class Todo {
     this.note.push(note);
   }
 }
+
+export default function displayTodo() {
+  getTodoForm();
+}
+
+function getTodoForm() {
+  const inputTitle = document.getElementById("title");
+  const inputDescription = document.getElementById("description");
+  const inputDueDate = document.getElementById("dueDate");
+  const inputPriority = document.getElementById("priority");
+  const inputNote = document.getElementById("note");
+  const submitButton = document.getElementById("submitButton");
+
+  submitButton.addEventListener("click", () => {
+    addTodoToMyTodos(
+      inputTitle,
+      inputDescription,
+      inputDueDate,
+      inputPriority,
+      inputNote
+    );
+    console.log("click");
+  });
+}
+
+function addTodoToMyTodos(title, description, dueDate, priority, note) {
+  myTodos.push(
+    new Todo(
+      title.value,
+      description.value,
+      dueDate.value,
+      priority.value,
+      note.value
+    )
+  );
+  displayMiddleContentTodos();
+}
+function displayMiddleContentTodos() {
+  createMiddleContentTodo();
+}
+function createMiddleContentTodo() {
+  const bottomDisplay = document.getElementById("bottomDisplay");
+  bottomDisplay.innerHTML = "";
+  myTodos.forEach((todos, index) => {
+    const todoButton = document.createElement("button");
+    todoButton.textContent = todos.title;
+    bottomDisplay.append(todoButton);
+  });
+}
