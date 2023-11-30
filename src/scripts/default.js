b096ac055a88d8407978c7a23ae636fe17da64cf
lym <aguineaolymm@gmail.com>
2023-11-30 17:16:48 +0800

    Created 3 container for contents, top, middle, and bottom container

--- src/scripts/default.js
+++ src/scripts/default.js
@@ -1,4 +1,56 @@
+import createTodoPage from "./createTodoPage";
+
 export default function showDefaultContent() {
+  showDefaultDisplay();
+}
+
+function showDefaultDisplay() {
+  createTopDisplay();
+  createMiddleDisplay();
+  createBottomDisplay();
+  displayTodoForm();
+}
+function createTopDisplay() {
+  let projectCount = 0;
   const mainContent = document.getElementById("mainContent");
-  mainContent.textContent = "MAIN CONTENT CLICKED";
+  const display = document.createElement("div");
+  display.id = "currentProjectContainer";
+  const currentProjectLocation = document.createElement("h2");
+  currentProjectLocation.textContent = "All Projects";
+  const currentProjectCounts = document.createElement("h2");
+  currentProjectCounts.textContent = projectCount;
+  mainContent.append(display);
+  display.append(currentProjectLocation);
+  display.append(currentProjectCounts);
+}
+function createMiddleDisplay() {
+  const mainContent = document.getElementById("mainContent");
+  const middleDisplay = document.createElement("div");
+  middleDisplay.id = "middleDisplay";
+
+  const addTodoButton = document.createElement("button");
+  addTodoButton.id = "addTodoButton";
+  addTodoButton.textContent = "New TODO";
+
+  mainContent.append(middleDisplay);
+  middleDisplay.append(addTodoButton);
+}
+function createBottomDisplay() {
+  const mainContent = document.getElementById("mainContent");
+  const bottomDisplay = document.createElement("div");
+  bottomDisplay.id = "bottomDisplay";
+
+  mainContent.append(bottomDisplay);
+}
+function clearTodoPage() {
+  const todoPage = document.getElementById("todoPage");
+  todoPage.innerHTML = "";
+}
+
+function displayTodoForm() {
+  const addTodoButton = document.getElementById("addTodoButton");
+  addTodoButton.addEventListener("click", () => {
+    clearTodoPage();
+    createTodoPage();
+  });
 }
