ad0759568d809c31b9e0a1b3d8cd0c573e808ccb
0lym <aguineaolymm@gmail.com>
2023-12-06 17:24:58 +0800

    Refactored and added checker for todayToday.js

--- src/scripts/isComplete.js
+++ src/scripts/isComplete.js
@@ -6,18 +6,34 @@ export function getTodosIsCheck() {
     todoChecker(isComplete, index);
   });
 }
+export function getTodayTodoIsCheck() {
+  const todayDate = formattedTodayDate();
+  dateFilterPriority(todayDate);
+}
+function formattedTodayDate() {
+  const getDate = new Date();
+  const year = getDate.getFullYear().toString();
+  const month = (getDate.getMonth() + 1).toString().padStart(2, "0");
+  const day = getDate.getDate().toString().padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+function dateFilterPriority(todayDate) {
+  myTodos.forEach((todo, index) => {
+    if (todo.dueDate === todayDate) {
+      const isComplete = todo.isComplete;
+      todoChecker(isComplete, index);
+    }
+  });
+}
+
 function todoChecker(isComplete, index) {
-  if (isComplete === true) {
-    taskCompleteTrue(index);
-  } else if (isComplete === false) {
-    taskCompleteFalse(index);
+  if (isComplete) {
+    taskCompleteStatus(index, true);
+  } else {
+    taskCompleteStatus(index, false);
   }
 }
-function taskCompleteTrue(index) {
+function taskCompleteStatus(index, status) {
   const button = document.querySelector(`[data-todoid="${index}"]`);
-  button.classList.add("taskCompleteTrue");
-}
-function taskCompleteFalse(index) {
-  const button = document.querySelector(`[data-todoid="${index}"]`);
-  button.classList.remove("taskCompleteTrue");
+  button.classList.toggle("taskCompleteTrue", status);
 }
