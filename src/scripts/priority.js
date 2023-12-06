b9f41a53a1cd9f03db7411e53610560d06666a66
0lym <aguineaolymm@gmail.com>
2023-12-06 17:25:44 +0800

    refactored and add a checker for todayTodo.js

--- src/scripts/priority.js
+++ src/scripts/priority.js
@@ -1,32 +1,53 @@
 import { myTodos } from "./todo";
+
 const HIGH_PRIORITY = "High priority";
 const MEDIUM_PRIORITY = "Medium priority";
 const LOW_PRIORITY = "Low priority";
 
-export function getTodoPriorty() {
+function addPriorityButtonClass(index, priorityClass) {
+  const button = document.querySelector(`[data-todoid="${index}"]`);
+  button.classList.add(priorityClass);
+}
+
+export function getTodoPriority() {
   myTodos.forEach((todo, index) => {
     const priority = todo.priority;
-    filterPriorty(priority, index);
+    filterPriority(priority, index);
   });
 }
-function filterPriorty(priority, index) {
-  if (priority === HIGH_PRIORITY) {
-    addHighPriortyButtonClass(index);
-  } else if (priority === MEDIUM_PRIORITY) {
-    addMediumPriortyButtonClass(index);
-  } else if (priority === LOW_PRIORITY) {
-    addLowPriortyButtonClass(index);
+
+export function getTodayTodoPriority() {
+  const getDate = new Date();
+  const year = getDate.getFullYear().toString();
+  const month = (getDate.getMonth() + 1).toString().padStart(2, "0");
+  const day = getDate.getDate().toString().padStart(2, "0");
+  const todayDate = `${year}-${month}-${day}`;
+
+  dateFilterPriority(todayDate);
+}
+
+function dateFilterPriority(todayDate) {
+  myTodos.forEach((todo, index) => {
+    if (todo.dueDate === todayDate) {
+      const priority = todo.priority;
+      filterPriority(priority, index);
+    }
+  });
+}
+
+function filterPriority(priority, index) {
+  switch (priority) {
+    case HIGH_PRIORITY:
+      addPriorityButtonClass(index, "high");
+      break;
+    case MEDIUM_PRIORITY:
+      addPriorityButtonClass(index, "medium");
+      break;
+    case LOW_PRIORITY:
+      addPriorityButtonClass(index, "low");
+      break;
+    default:
+      // Handle other cases if necessary
+      break;
   }
-}
-function addHighPriortyButtonClass(index) {
-  const button = document.querySelector(`[data-todoid="${index}"]`);
-  button.classList.add("high");
-}
-function addMediumPriortyButtonClass(index) {
-  const button = document.querySelector(`[data-todoid="${index}"]`);
-  button.classList.add("medium");
-}
-function addLowPriortyButtonClass(index) {
-  const button = document.querySelector(`[data-todoid="${index}"]`);
-  button.classList.add("low");
 }
