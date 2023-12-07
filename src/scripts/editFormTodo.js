289a9a001a947c163219f7738c5f4eab5d24f31c
0lym <aguineaolymm@gmail.com>
2023-12-07 17:29:49 +0800

    create 2 edit form function, 1 for default, 1 for today todo

--- src/scripts/editFormTodo.js
+++ src/scripts/editFormTodo.js
@@ -1,14 +1,13 @@
 import { myTodos } from "./todo";
 import { Todo } from "./todo";
 import { displayMiddleContentTodos } from "./todo";
+
 export function getTodoFormToBeEdited(todos, index) {
   const inputTitle = document.getElementById("title");
   const inputDescription = document.getElementById("description");
   const inputDueDate = document.getElementById("dueDate");
   const inputPriority = document.getElementById("priority");
   const inputNote = document.getElementById("note");
-  const submitButton = document.getElementById("submitButton");
-
   inputTitle.value = todos.title;
   inputDescription.value = todos.description;
   inputDueDate.value = todos.dueDate;
@@ -32,6 +31,49 @@ export function getTodoFormToBeEdited(todos, index) {
       displayMiddleContentTodos();
     }
   });
+}
+export function getTodayFormEdited(todos, index) {
+  const inputTitle = document.getElementById("title");
+  const inputDescription = document.getElementById("description");
+  const inputDueDate = document.getElementById("dueDate");
+  disabledTodayDueDate();
+  const inputPriority = document.getElementById("priority");
+  const inputNote = document.getElementById("note");
+  inputTitle.value = todos.title;
+  inputDescription.value = todos.description;
+  inputDueDate.value = todos.dueDate;
+  inputPriority.value = todos.priority;
+  inputNote.value = todos.note;
+
+  submitButton.addEventListener("click", () => {
+    if (inputTitle.value === "" || inputDueDate.value === "") {
+      console.log("error");
+    } else {
+      myTodos.splice(index, 1);
+      addEditedTodotoMyTodos(
+        inputTitle.value,
+        inputDescription.value,
+        inputDueDate.value,
+        inputPriority.value,
+        inputNote.value
+      );
+      console.log("click");
+      clearTodoPage();
+      displayMiddleContentTodos();
+    }
+  });
+}
+
+function disabledTodayDueDate() {
+  const dueDate = document.getElementById("dueDate");
+  dueDate.disabled = true;
+}
+function formattedTodayDate() {
+  const getDate = new Date();
+  const year = getDate.getFullYear().toString();
+  const month = (getDate.getMonth() + 1).toString().padStart(2, "0");
+  const day = getDate.getDate().toString().padStart(2, "0");
+  return `${year}-${month}-${day}`;
 }
 function clearTodoPage() {
   const todoPage = document.getElementById("todoPage");
