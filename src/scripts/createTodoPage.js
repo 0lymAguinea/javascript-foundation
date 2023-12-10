738092966dfe79e86407889d25755bbf19d33936
lym <aguineaolymm@gmail.com>
2023-12-10 16:21:33 +0800

    added current day function as minimum value for inputDueDate

--- src/scripts/createTodoPage.js
+++ src/scripts/createTodoPage.js
@@ -52,7 +52,13 @@ function createFormLabels() {
   labels.append(labelPriority);
   labels.append(labelNote);
 }
-
+function formattedTodayDate() {
+  const getDate = new Date();
+  const year = getDate.getFullYear().toString();
+  const month = (getDate.getMonth() + 1).toString().padStart(2, "0");
+  const day = getDate.getDate().toString().padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
 function createFormInput() {
   const labelTitle = document.getElementById("labelTitle");
   const labelDescription = document.getElementById("labelDescription");
@@ -75,6 +81,7 @@ function createFormInput() {
   inputDueDate.type = "date";
   inputDueDate.name = "dueDate";
   inputDueDate.id = "dueDate";
+  inputDueDate.min = formattedTodayDate();
 
   const inputNote = document.createElement("input");
   inputNote.type = "text";
