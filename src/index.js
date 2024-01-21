f75c8d0eb4487dff270c1311f20332b3b57a6599
Lym <aguineaolymm@gmail.com>
2024-01-21 15:14:43 +0800

    Create gameBoard class with boardMap function and placeShip function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 class Ship {
   constructor(name) {
     this.name = name;
@@ -48,8 +49,72 @@ class Ship {
   }
 }
 
-const player1 = new Ship();
-const player2 = new Ship();
+class Gameboard {
+  constructor(ship) {
+    this.ship = ship;
+    this.board = this.boardMap();
+  }
 
-console.log(player1);
-console.log(player2);
+  boardMap() {
+    const row = 10;
+    const col = 10;
+    const board = [];
+
+    for (let i = 0; i < row; i += 1) {
+      board[i] = [];
+      for (let j = 0; j < col; j += 1) {
+        board[i][j] = 0;
+      }
+    }
+
+    return board;
+  }
+
+  placeShip(coordinateX, coordinateY, ship, orientation) {
+    if (
+      this.isValidCoordinate(coordinateX, coordinateY) &&
+      this.isValidShipPlacement(coordinateX, coordinateY, ship, orientation)
+    ) {
+      for (let i = 0; i < ship.length; i += 1) {
+        if (orientation === "horizontal") {
+          this.board[coordinateX][coordinateY + i] = ship.length;
+        } else if (orientation === "vertical") {
+          this.board[coordinateX + i][coordinateY] = ship.length;
+        }
+      }
+    } else {
+      console.log("Invalid placement");
+    }
+  }
+
+  isValidCoordinate(coordinateX, coordinateY) {
+    const row = this.board.length;
+    const col = this.board[0].length;
+    return (
+      coordinateX >= 0 &&
+      coordinateX < row &&
+      coordinateY >= 0 &&
+      coordinateY < col
+    );
+  }
+
+  isValidShipPlacement(coordinateX, coordinateY, ship, orientation) {
+    if (orientation === "horizontal") {
+      return coordinateY + ship.length <= this.board[0].length;
+    }
+    if (orientation === "vertical") {
+      return coordinateX + ship.length <= this.board.length;
+    }
+  }
+}
+
+const player1Ships = new Ship();
+const player2Ships = new Ship();
+
+const player1Board = new Gameboard(player1Ships);
+const player2Board = new Gameboard(player2Ships);
+
+player1Board.placeShip(1, 5, player1Ships.shipCategory.carrier, "horizontal");
+
+console.log(player1Board);
+console.log(player2Board);
