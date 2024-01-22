4047f41388bc3fbef30a5138eb58d6d16a6029a8
Lym <aguineaolymm@gmail.com>
2024-01-22 13:42:33 +0800

    Moved ship class to its own js file

--- src/index.js
+++ src/index.js
@@ -1,56 +1,5 @@
 /* eslint-disable max-classes-per-file */
-class Ship {
-  constructor() {
-    this.shipCategory = {
-      carrier: {
-        name: "carrier",
-        length: 5,
-        numberOfHits: 0,
-        sunk: false,
-      },
-      battleship: {
-        name: "battleship",
-        length: 4,
-        numberOfHits: 0,
-        sunk: false,
-      },
-      cruiser: {
-        name: "cruiser",
-        length: 3,
-        numberOfHits: 0,
-        sunk: false,
-      },
-      submarine: {
-        name: "submarine",
-        length: 3,
-        numberOfHits: 0,
-        sunk: false,
-      },
-      destroyer: {
-        name: "destroyer",
-        length: 2,
-        numberOfHits: 0,
-        sunk: false,
-      },
-    };
-  }
-
-  hit(shipType) {
-    if (
-      Object.hasOwn(this.shipCategory, shipType) &&
-      this.shipCategory[shipType].sunk !== true
-    ) {
-      this.shipCategory[shipType].numberOfHits += 1;
-      this.isSunk(this.shipCategory, shipType);
-    }
-  }
-
-  isSunk(hitShip, shipType) {
-    if (hitShip[shipType].numberOfHits === hitShip[shipType].length) {
-      hitShip[shipType].sunk = true;
-    }
-  }
-}
+import Ship from "./scripts/ship";
 
 class Gameboard {
   constructor(ship) {
@@ -162,7 +111,7 @@ class Gameboard {
     if (
       array.map((ship) => ship.sunk).every((sunk) => sunk === true) === true
     ) {
-      console.log("WINNER!");
+      console.log("ALL SHIP SUNK!");
     } else {
       console.log("NOT YET");
     }
---
+++ src/scripts/ship.js
@@ -0,0 +1,52 @@
+export default class Ship {
+  constructor() {
+    this.shipCategory = {
+      carrier: {
+        name: "carrier",
+        length: 5,
+        numberOfHits: 0,
+        sunk: false,
+      },
+      battleship: {
+        name: "battleship",
+        length: 4,
+        numberOfHits: 0,
+        sunk: false,
+      },
+      cruiser: {
+        name: "cruiser",
+        length: 3,
+        numberOfHits: 0,
+        sunk: false,
+      },
+      submarine: {
+        name: "submarine",
+        length: 3,
+        numberOfHits: 0,
+        sunk: false,
+      },
+      destroyer: {
+        name: "destroyer",
+        length: 2,
+        numberOfHits: 0,
+        sunk: false,
+      },
+    };
+  }
+
+  hit(shipType) {
+    if (
+      Object.hasOwn(this.shipCategory, shipType) &&
+      this.shipCategory[shipType].sunk !== true
+    ) {
+      this.shipCategory[shipType].numberOfHits += 1;
+      this.isSunk(this.shipCategory, shipType);
+    }
+  }
+
+  isSunk(hitShip, shipType) {
+    if (hitShip[shipType].numberOfHits === hitShip[shipType].length) {
+      hitShip[shipType].sunk = true;
+    }
+  }
+}
