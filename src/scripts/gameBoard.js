b4d820eff55cacdf450abd169b559b5cc5e0b94e
Lym <aguineaolymm@gmail.com>
2024-01-23 13:58:47 +0800

    Moved gameboard to a module

---
+++ src/scripts/gameBoard.js
@@ -0,0 +1,141 @@
+export default class Gameboard {
+  constructor(ship) {
+    this.ship = ship;
+    this.board = this.boardMap();
+    this.shotsFiredCount = 0;
+    this.missedAttackCount = 0;
+  }
+
+  boardMap() {
+    const row = 10;
+    const col = 10;
+    const board = [];
+
+    for (let i = 0; i < row; i += 1) {
+      board[i] = [];
+      for (let j = 0; j < col; j += 1) {
+        board[i][j] = 0;
+      }
+    }
+
+    return board;
+  }
+
+  placeShip(coordinateX, coordinateY, ship, orientation) {
+    if (
+      this.isValidCoordinate(coordinateX, coordinateY) &&
+      this.isValidShipPlacement(coordinateX, coordinateY, ship, orientation)
+    ) {
+      for (let i = 0; i < ship.length; i += 1) {
+        if (orientation === "horizontal") {
+          this.board[coordinateX][coordinateY + i] = ship.name;
+        } else if (orientation === "vertical") {
+          this.board[coordinateX + i][coordinateY] = ship.name;
+        }
+      }
+    } else {
+      console.log("Invalid placement");
+    }
+  }
+
+  isValidCoordinate(coordinateX, coordinateY) {
+    const row = this.board.length;
+    const col = this.board[0].length;
+    return (
+      coordinateX >= 0 &&
+      coordinateX < row &&
+      coordinateY >= 0 &&
+      coordinateY < col
+    );
+  }
+
+  isValidShipPlacement(coordinateX, coordinateY, ship, orientation) {
+    if (orientation === "horizontal") {
+      return coordinateY + ship.length <= this.board[0].length;
+    }
+    if (orientation === "vertical") {
+      return coordinateX + ship.length <= this.board.length;
+    }
+  }
+
+  receiveAttack(coordinateX, coordinateY, playerShip) {
+    if (
+      this.board[coordinateX][coordinateY] === "X" ||
+      this.board[coordinateX][coordinateY] === "O"
+    ) {
+      console.log("YOU ALREADY HIT THIS");
+    } else if (
+      this.board[coordinateX][coordinateY] !== undefined &&
+      this.board[coordinateX][coordinateY] !== 0
+    ) {
+      const shipHit = this.board[coordinateX][coordinateY];
+      this.board[coordinateX][coordinateY] = "X";
+      this.ship.hit(shipHit);
+      this.shotsFired(playerShip);
+    } else {
+      this.board[coordinateX][coordinateY] = "O";
+      this.shotsFired(playerShip);
+      this.missedAttack(playerShip);
+    }
+  }
+
+  shotsFired(playerShip) {
+    playerShip.shotsFiredCount += 1;
+
+    return playerShip.shotsFiredCount;
+  }
+
+  missedAttack(playerShip) {
+    playerShip.missedAttackCount += 1;
+
+    return playerShip.missedAttackCount;
+  }
+
+  getAccuracy() {
+    const totalShots = this.shotsFiredCount + this.missedAttackCount;
+
+    if (totalShots === 0) {
+      return 0;
+    }
+
+    const hitRatio = this.shotsFiredCount / totalShots;
+
+    return hitRatio;
+  }
+
+  checkIfAllShipSunk() {
+    const array = Object.values(this.ship.shipCategory);
+    if (
+      array.map((ship) => ship.sunk).every((sunk) => sunk === true) === true
+    ) {
+      console.log("ALL SHIP SUNK!");
+    } else {
+      console.log("NOT YET");
+    }
+  }
+
+  checkplayerTurn(player1, player2) {
+    if (player1.turn === true && player2.turn === false) {
+      const coordinates = prompt("Player 1, choose coordinates");
+      const answer = coordinates.split(",");
+      const row = answer[0];
+      const col = answer[1];
+
+      player2Board.receiveAttack(row, col, player1Board);
+      this.switchPlayer(player1, player2);
+    } else if (player1.turn === false && player2.turn === true) {
+      const coordinates = prompt("Player 2, choose coordinates");
+      const answer = coordinates.split(",");
+      const row = answer[0];
+      const col = answer[1];
+
+      player1Board.receiveAttack(row, col, player2Board);
+      this.switchPlayer(player1, player2);
+    }
+  }
+
+  switchPlayer(player1, player2) {
+    player1.turn = !player1.turn;
+    player2.turn = !player2.turn;
+  }
+}
