ad22f8b6c6ce11ae1ca5c688f2fad7894c2bd81a
Lym <aguineaolymm@gmail.com>
2024-01-24 15:41:56 +0800

    Create buttons functions for clicking and registering the attacks and missed at the gameboard

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import Ship from "./scripts/ship";
 import Player from "./scripts/player";
 import Gameboard from "./scripts/gameBoard";
 
-function gameLoop(player1Ships, player2Ships) {
-  while (!areAllShipSunk(player1Ships) && !areAllShipSunk(player2Ships)) {
-    player1Board.checkplayerTurn(player1, player2);
-    player2Board.checkplayerTurn(player1, player2);
-  }
-}
-
 function areAllShipSunk(playerShip) {
   const ships = Object.values(playerShip.shipCategory);
   return ships.every((ship) => ship.sunk);
@@ -30,16 +23,13 @@ const player2Cruiser = player2Ships.shipCategory.cruiser;
 const player2Submarine = player2Ships.shipCategory.submarine;
 const player2Destroyer = player2Ships.shipCategory.destroyer;
 
-const player1Board = new Gameboard(player1Ships);
-const player2Board = new Gameboard(player2Ships);
+export const player1Board = new Gameboard(player1Ships);
+export const player2Board = new Gameboard(player2Ships);
 
-player1Board.placeShip(1, 5, player1Carrier, "horizontal");
-player2Board.placeShip(2, 5, player2Destroyer, "horizontal");
+player1Board.placeShip(0, 5, player1Carrier, "horizontal");
+player2Board.placeShip(2, 5, player2Battleship, "horizontal");
 const player1 = new Player("Lym", true);
 const player2 = new Player("Myl", false);
-
-console.log(player1Board);
-console.log(player2Board);
 
 function displayPlayer1Board() {
   const display = document.getElementById("player1Board");
@@ -50,7 +40,7 @@ function displayPlayer1Board() {
       const button = document.createElement("button");
       button.dataset.row = i;
       button.dataset.column = j;
-      button.classList = "player1Cell";
+      button.classList = "player1Buttons";
       display.appendChild(button);
     }
   }
@@ -65,29 +55,61 @@ function displayPlayer2Board() {
       const button = document.createElement("button");
       button.dataset.row = i;
       button.dataset.column = j;
-      button.classList = "player2Cell";
+      button.classList = "player2Buttons";
       display.appendChild(button);
     }
   }
 }
 
-function player1BoardButtonController() {
-  const buttons = document.querySelectorAll(".player1Cell");
-
+function buttonsController(
+  buttons,
+  currentBoard,
+  opposingBoard,
+  currentPlayer,
+  opposingPlayer
+) {
   buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-      const row = e.target.getAttribute("data-row");
-      const column = e.target.getAttribute("data-column");
+      if (currentPlayer.turn) {
+        const row = e.target.getAttribute("data-row");
+        const column = e.target.getAttribute("data-column");
 
-      player2Board.receiveAttack(row, column, player1Board);
+        opposingBoard.receiveAttack(row, column, currentBoard, button);
+        currentBoard.switchPlayer(currentPlayer, opposingPlayer);
 
-      console.log(player1Board);
-      console.log(player2Board);
+        button.disabled = true;
+
+        initializeButtons();
+        console.log(player1Board, player2Board);
+      }
     });
   });
+}
+
+function initializeButtons() {
+  const player1Buttons = document.querySelectorAll(".player1Buttons");
+  const player2Buttons = document.querySelectorAll(".player2Buttons");
+
+  if (player1.turn === true && player2.turn === false) {
+    buttonsController(
+      player1Buttons,
+      player1Board,
+      player2Board,
+      player1,
+      player2
+    );
+  } else if (player1.turn === false && player2.turn === true) {
+    buttonsController(
+      player2Buttons,
+      player2Board,
+      player1Board,
+      player2,
+      player1
+    );
+  }
 }
 
 displayPlayer1Board();
 displayPlayer2Board();
 
-player1BoardButtonController();
+initializeButtons();
